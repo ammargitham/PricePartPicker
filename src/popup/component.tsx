b36b0ef491bf30b6d4f9ae6a3d73505d65722028
Ammar Githam <ammar.githam@outlook.com>
2022-12-08 22:03:46 +0900

    Remove unused scroller component

--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -1,55 +1,8 @@
 import React from 'react';
 
-import browser, { Tabs } from 'webextension-polyfill';
-
-import { Scroller } from '@src/components/scroller';
+import browser from 'webextension-polyfill';
 
 import css from './styles.module.css';
-
-// // // //
-
-// Scripts to execute in current tab
-const scrollToTopPosition = 0;
-const scrollToBottomPosition = 9999999;
-
-function scrollWindow(position: number) {
-  window.scroll(0, position);
-}
-
-/**
- * Executes a string of Javascript on the current tab
- * @param code The string of code to execute on the current tab
- */
-function executeScript(position: number): void {
-  // Query for the active tab in the current window
-  browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then((tabs: Tabs.Tab[]) => {
-      // Pulls current tab from browser.tabs.query response
-      const currentTab: Tabs.Tab | number = tabs[0];
-
-      // Short circuits function execution is current tab isn't found
-      if (!currentTab) {
-        return;
-      }
-      const currentTabId: number = currentTab.id as number;
-
-      // Executes the script in the current tab
-      browser.scripting
-        .executeScript({
-          target: {
-            tabId: currentTabId,
-          },
-          func: scrollWindow,
-          args: [position],
-        })
-        .then(() => {
-          // console.log('Done Scrolling');
-        });
-    });
-}
-
-// // // //
 
 export function Popup() {
   // Sends the `popupMounted` event
@@ -62,14 +15,6 @@ export function Popup() {
     <div className={css.popupContainer}>
       <div className="mx-4 my-4">
         <hr />
-        <Scroller
-          onClickScrollTop={() => {
-            executeScript(scrollToTopPosition);
-          }}
-          onClickScrollBottom={() => {
-            executeScript(scrollToBottomPosition);
-          }}
-        />
       </div>
     </div>
   );
